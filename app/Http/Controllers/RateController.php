a37dddb3ed382357c3185a068c57e1748dc61a19
Sanzeeb Aryal <san_zib@live.com>
2016-11-06 16:17:55 +0545

    lara5.3 new start

---
+++ app/Http/Controllers/RateController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use Socialite;
+
+
+class RateController extends Controller
+{
+    public function index()
+	{
+		return view('rate.index');
+	}
+}
